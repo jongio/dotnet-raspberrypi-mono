1bc39eb9305cb473ee879d71f90e6d9b12ccadba
jongallant <jonbgallant@gmail.com>
2014-12-19 10:47:28 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Raspberry.IO.GeneralPurpose;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotnet_raspberrypi_mono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputPinConfiguration pin12 = ConnectorPin.P1Pin12.Output();
+            pin12.Enabled = false;
+
+            using (GpioConnection connection = new GpioConnection(pin12))
+            {
+                while (!Console.KeyAvailable)
+                {
+                    connection.Toggle(pin12);
+                    Thread.Sleep(250);
+                }
+            }
+        }
+    }
+}
